c17b330a55c39e4cef1e06f365a979f51c20560e
Pascal Mpawenayo <pm@gmail.com>
2017-02-10 22:12:13 -0500

    set up project file and create .gitignore

---
+++ Test/com/pascalmp/SinkingSortTest.java
@@ -0,0 +1,14 @@
+package com.pascalmp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SinkingSortTest {
+
+	@Test
+	public void shouldSortSortedArrayList() {
+		assertArrayEquals(new int[] {0, 1}, SinkingSort.sortArrayList(new int[] {0, 1}));
+	}
+
+}
---
+++ src/com/pascalmp/SinkingSort.java
@@ -0,0 +1,10 @@
+package com.pascalmp;
+
+public class SinkingSort {
+
+	public static int[] sortArrayList(int[] is) {
+		// TODO Auto-generated method stub
+		return is;
+	}
+
+}
